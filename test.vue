9adebe9f017a15e4a4c179ce59b7c79174153f23
KulPlant <kulplant@gmail.com>
2017-11-17 14:48:03 +0100

    test

---
+++ test.vue
@@ -0,0 +1,91 @@
+<!-- Tufts VUE 3.3.0 concept-map (test.vue) 2017-11-17 -->
+<!-- Tufts VUE: http://vue.tufts.edu/ -->
+<!-- Do Not Remove: VUE mapping @version(1.1) jar:file:/C:/Program%20Files%20(x86)/VUE/VUE.jar!/tufts/vue/resources/lw_mapping_1_1.xml -->
+<!-- Do Not Remove: Saved date Fri Nov 17 14:47:46 CET 2017 by Kul on platform Windows 8 6.2 in JVM 1.7.0_21-b11 -->
+<!-- Do Not Remove: Saving version @(#)VUE: built October 8 2015 at 1658 by tomadm on Linux 2.6.32-504.23.4.el6.x86_64 i386 JVM 1.7.0_21-b11(bits=32) -->
+<?xml version="1.0" encoding="US-ASCII"?>
+<LW-MAP xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xsi:noNamespaceSchemaLocation="none" ID="0" label="test.vue"
+    created="1510922080205" x="0.0" y="0.0" width="1.4E-45"
+    height="1.4E-45" strokeWidth="0.0" autoSized="false">
+    <resource referenceCreated="1510926466660"
+        spec="D:\Art\My-Making-Game\NotesofHeartsVUE\test.vue" type="1" xsi:type="URLResource">
+        <title>test.vue</title>
+        <property key="File" value="D:\Art\My-Making-Game\NotesofHeartsVUE\test.vue"/>
+    </resource>
+    <fillColor>#FFFFFF</fillColor>
+    <strokeColor>#404040</strokeColor>
+    <textColor>#000000</textColor>
+    <font>SansSerif-plain-14</font>
+    <URIString>http://vue.tufts.edu/rdf/resource/ca3c3e6ac3f9b91c01c7e656a530f821</URIString>
+    <child ID="6" label="test" layerID="1" created="1510922084556"
+        x="546.0" y="196.0" width="297.0" height="162.0"
+        strokeWidth="1.0" autoSized="false" xsi:type="node">
+        <fillColor>#A6A6A6</fillColor>
+        <strokeColor>#000000</strokeColor>
+        <textColor>#000000</textColor>
+        <font>Arial-plain-12</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/ca3c3e6ac3f9b91c01c7e65639901d5b</URIString>
+        <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
+    </child>
+    <layer ID="1" label="Layer 1" created="1510922080205" x="0.0"
+        y="0.0" width="1.4E-45" height="1.4E-45" strokeWidth="0.0" autoSized="false">
+        <URIString>http://vue.tufts.edu/rdf/resource/ca3c3e6bc3f9b91c01c7e65667c5a84e</URIString>
+    </layer>
+    <userZoom>1.0</userZoom>
+    <userOrigin x="-14.0" y="-14.0"/>
+    <presentationBackground>#202020</presentationBackground>
+    <PathwayList currentPathway="0" revealerIndex="-1">
+        <pathway ID="0" label="&#x7121;&#x6a19;&#x984c;&#x8def;&#x5f91;"
+            created="1510922080205" x="0.0" y="0.0" width="1.4E-45"
+            height="1.4E-45" strokeWidth="0.0" autoSized="false"
+            currentIndex="-1" open="true">
+            <strokeColor>#B3CC33CC</strokeColor>
+            <textColor>#000000</textColor>
+            <font>SansSerif-plain-14</font>
+            <URIString>http://vue.tufts.edu/rdf/resource/ca3c3e6dc3f9b91c01c7e656538eaa00</URIString>
+            <masterSlide ID="2" created="1510922080226" x="0.0" y="0.0"
+                width="800.0" height="600.0" locked="true"
+                strokeWidth="0.0" autoSized="false">
+                <fillColor>#000000</fillColor>
+                <strokeColor>#404040</strokeColor>
+                <textColor>#000000</textColor>
+                <font>SansSerif-plain-14</font>
+                <URIString>http://vue.tufts.edu/rdf/resource/ca3c3e6dc3f9b91c01c7e656cfd70923</URIString>
+                <titleStyle ID="3" label="Header"
+                    created="1510922080236" x="335.5" y="171.0"
+                    width="129.0" height="58.0" strokeWidth="0.0"
+                    autoSized="true" isStyle="true" xsi:type="node">
+                    <strokeColor>#404040</strokeColor>
+                    <textColor>#FFFFFF</textColor>
+                    <font>Gill Sans-plain-36</font>
+                    <URIString>http://vue.tufts.edu/rdf/resource/ca3c3e6ec3f9b91c01c7e656da92686b</URIString>
+                    <shape xsi:type="rectangle"/>
+                </titleStyle>
+                <textStyle ID="4" label="Slide Text"
+                    created="1510922080237" x="346.5" y="280.5"
+                    width="107.0" height="39.0" strokeWidth="0.0"
+                    autoSized="true" isStyle="true" xsi:type="node">
+                    <strokeColor>#404040</strokeColor>
+                    <textColor>#FFFFFF</textColor>
+                    <font>Gill Sans-plain-22</font>
+                    <URIString>http://vue.tufts.edu/rdf/resource/ca3c3e6ec3f9b91c01c7e656a3fa6fae</URIString>
+                    <shape xsi:type="rectangle"/>
+                </textStyle>
+                <linkStyle ID="5" label="Links" created="1510922080238"
+                    x="373.5" y="383.0" width="53.0" height="34.0"
+                    strokeWidth="0.0" autoSized="true" isStyle="true" xsi:type="node">
+                    <strokeColor>#404040</strokeColor>
+                    <textColor>#B3BFE3</textColor>
+                    <font>Gill Sans-plain-18</font>
+                    <URIString>http://vue.tufts.edu/rdf/resource/ca3c3e6fc3f9b91c01c7e656fae46e4f</URIString>
+                    <shape xsi:type="rectangle"/>
+                </linkStyle>
+            </masterSlide>
+        </pathway>
+    </PathwayList>
+    <date>2017-11-17</date>
+    <modelVersion>6</modelVersion>
+    <saveLocation>D:\Art\My-Making-Game\NotesofHeartsVUE</saveLocation>
+    <saveFile>D:\Art\My-Making-Game\NotesofHeartsVUE\test.vue</saveFile>
+</LW-MAP>
